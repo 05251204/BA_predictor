550f164c972dce469d289c5698821b81f476e33e
05251204 <kazutora.komi@gmail.com>
2025-10-05 21:50:46 +0900

    fix: fetchするurlをvercelからrenderに変更

--- webapp/public/script.js
+++ webapp/public/script.js
@@ -15,7 +15,7 @@ charForm.addEventListener('submit', async (event) => {
     };
 
   try {
-        const response = await fetch('/api/predict-characters', {
+        const response = await fetch('https://ba-predictor-api.onrender.com/predict/characters', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(inputData)
